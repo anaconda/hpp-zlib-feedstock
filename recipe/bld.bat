d7e9fa10d1e17dcd4ec155ea30f5d6eb4fd5fc4b
Scott B <Scott@TheBaileyFamily.org>
2022-04-18 09:53:50 -0700

    Fix missing shared objects (#11)
    Changes:
    - Version remains `1.2.12`.
    - Update build number from `0` to `1`.
    - Convert [non win] from autotools/make to cmake/make.
    - Improve tests to ensure artifacts are in the right place.
    - Corrected license file and local contents.
    - Ensure ar tool path is set for `linux-aarch64`.
    
    Note: this is a version bump update to correct missing artifacts in
    the previous `1.2.12` package.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,22 +1,40 @@
+:: cmd
 set LIB=%LIBRARY_LIB%;%LIB%
 set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%;%RECIPE_DIR%
 
-:: Configure.
-cmake -G "NMake Makefiles" ^
-      -D CMAKE_BUILD_TYPE=Release ^
-      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
-      %SRC_DIR%
+:: Isolate the build.
+mkdir Build
+cd Build
+if errorlevel 1 exit /b 1
+
+
+:: Generate the build files.
+echo "Generating the build files."
+cmake .. %CMAKE_ARGS% ^
+      -G"NMake Makefiles" ^
+      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_BUILD_TYPE=Release
 if errorlevel 1 exit 1
+
 
 :: Build.
-cmake --build %SRC_DIR% --target INSTALL --config Release
-if errorlevel 1 exit 1
+echo "Building..."
+nmake
+if errorlevel 1 exit /b 1
 
-:: Test.
-ctest
-if errorlevel 1 exit 1
+
+:: Perforem tests.
+echo "Testing..."
+ctest -VV --output-on-failure
+
+
+:: Install.
+echo "Installing..."
+nmake install
+if errorlevel 1 exit /b 1
+
 
 :: Some OSS libraries are happier if z.lib exists, even though it's not typical on Windows.
 copy %LIBRARY_LIB%\zlib.lib %LIBRARY_LIB%\z.lib || exit 1
@@ -30,3 +48,7 @@ copy %RECIPE_DIR%\license.txt %SRC_DIR%\license.txt || exit 1
 :: python>=3.10 depend on this being at %PREFIX%
 copy %LIBRARY_BIN%\zlib.dll %PREFIX%\zlib.dll || exit 1
 
+
+:: Error free exit.
+echo "Error free exit!"
+exit 0
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Set flags
 export CFLAGS=$(echo ${CFLAGS} | sed 's|-O2|-O3|g')
 export CPPFLAGS=$(echo ${CPPFLAGS} | sed 's|-O2|-O3|g')
 
@@ -10,15 +11,48 @@ fi
 export CFLAGS="${CFLAGS} -fPIC"
 export CXXFLAGS="${CXXFLAGS} -fPIC"
 
-./configure --prefix=${PREFIX}  \
-            --shared
+# linux-aarch64 activations fails to set `ar` tool. This can be
+# removed when activations is corrected.
+if [[ "${target_platform}" == linux-aarch64 ]]; then
+  if [[ -n "$AR" ]]; then
+      CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_AR=${AR}"
+  fi
+fi
 
-make -j${CPU_COUNT} ${VERBOSE_AT}
-make check
-make install
+# Isolate the build.
+mkdir -p Build
+cd Build || exit 1
+
+
+# Generate the build files.
+echo "Generating the build files."
+cmake .. ${CMAKE_ARGS} \
+      -G"Unix Makefiles" \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+
+# Build.
+echo "Building..."
+make  -j${CPU_COUNT} || exit 1
+
+
+# Perform tests.
+echo "Testing..."
+ctest -VV --output-on-failure || exit 1
+
+
+# Installing
+echo "Installing..."
+make install || exit 1
 
 # Remove man files.
 rm -rf $PREFIX/share
 
 # Copy license file to the source directory so conda-build can find it.
 cp $RECIPE_DIR/license.txt $SRC_DIR/license.txt
+
+
+# Error free exit!
+echo "Error free exit!"
+exit 0
